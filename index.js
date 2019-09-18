070e0c4a6c0419167c1bc88082c6ddb2a5f664ac
AndrewA0112 <andrew.mkal@outlook.com>
2019-09-18 17:11:19 -0500

    Setting up heroku

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 const server = require('./api/server.js');
+const defaults = require('./config/defaults.js')
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+server.listen(defaults.port, () => console.log(`\n** Running on port ${defaults.port} **\n`));
